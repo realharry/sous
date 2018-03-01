15253855f4d4418dd04267631b4923f253810299
Judson <nyarly@gmail.com>
2018-03-01 10:33:27 -0800

    Added a mixed candidate/non-candidate test

--- util/sqlgen/fieldset_test.go
+++ util/sqlgen/fieldset_test.go
@@ -46,3 +46,50 @@ func TestRowDedupe_without_candidates(t *testing.T) {
 		t.Errorf("Expected 3 values, got %d", len(vals))
 	}
 }
+
+func TestRowDedupe_mixed_fields(t *testing.T) {
+	fs := NewFieldset()
+
+	fs.Row(func(rd RowDef) {
+		rd.CF("?", "candidate", 1)
+		rd.FD("?", "other", "something")
+	})
+	fs.Row(func(rd RowDef) {
+		rd.CF("?", "candidate", 1)
+		// note that non-candidate fields aren't considered for de-duplications
+		// that's what a candidate field means in this context, though.
+		rd.FD("?", "other", "or other")
+	})
+	fs.Row(func(rd RowDef) {
+		rd.CF("?", "candidate", 2)
+		rd.FD("?", "other", "also this")
+	})
+
+	sql := fs.InsertSQL("testtable", "")
+	expectedSQL := "insert into testtable (candidate,other) values ($1,$2),\n($3,$4) "
+	if sql != expectedSQL {
+		t.Errorf("Expected fieldset to generate %q; got %q", expectedSQL, sql)
+	}
+
+	vals := fs.InsertValues()
+	if len(vals) != 4 {
+		t.Errorf("Expected 4 values, got %d", len(vals))
+	}
+	firstC := vals[0]
+	expectedC := 1
+	if firstC != expectedC {
+		t.Errorf("Expected first candidate to be %v; got %v", expectedC, firstC)
+	}
+
+	firstNC := vals[1]
+	expectedNC := "something"
+	if firstNC != expectedNC {
+		t.Errorf("Expected first non-candidate to be %v; got %v", expectedNC, firstNC)
+	}
+
+	secondNC := vals[3]
+	expectedNC = "also this"
+	if secondNC != expectedNC {
+		t.Errorf("Expected other non-candidate to be %v; got %v", expectedNC, secondNC)
+	}
+}
