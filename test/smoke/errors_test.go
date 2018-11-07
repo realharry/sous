2115870264652854670eaaf0d61b18c6b48990de
Sam Salisbury <samsalisbury@gmail.com>
2018-11-07 11:01:01 +0000

    test/smoke: errors: sousdeploy-notag-nogittag
    - Failing test for new error message.

---
+++ test/smoke/errors_test.go
@@ -0,0 +1,24 @@
+package smoke
+
+import (
+	"strings"
+	"testing"
+)
+
+// TestErrors tests for error messages.
+func TestErrors(t *testing.T) {
+
+	m := newRunner(t, matrix())
+
+	m.Run("sousdeploy-notagflag-nogittag", func(t *testing.T, f *fixture) {
+		p := setupProject(t, f, f.Projects.HTTPServer())
+
+		stderr := p.MustFail(t, "deploy", nil, "-cluster", "cluster1")
+
+		want := "you must provide the -tag flag (no git tag found)"
+
+		if !strings.Contains(stderr, want) {
+			t.Errorf("got stderr %q; want it to contain %q", stderr, want)
+		}
+	})
+}
